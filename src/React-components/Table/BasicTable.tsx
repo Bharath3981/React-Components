d5e6df65573cff8df511fa5ae4799bb9ebfabb2f
Bharath3981 <bharath.baisetty@gmail.com>
2023-09-30 15:22:20 +0530

    Table upd

--- src/React-components/Table/BasicTable.tsx
+++ src/React-components/Table/BasicTable.tsx
@@ -2,16 +2,72 @@ import { useFetchBasicTableDataQuery } from "../../Slices/TableApiSlice";
 import RcTable from "./RcTable";
 
 const BasicTable = () => {
-  const { data = [], isFetching } = useFetchBasicTableDataQuery(null);
-  console.log(data);
+  const { data = [], isSuccess } = useFetchBasicTableDataQuery(null);
+  const options = {};
+  const columns = [
+    {
+      label: "Department Id",
+      field: "DepartmentId",
+    },
+    {
+      label: "Department Name",
+      field: "DepartmentName",
+    },
+    {
+      label: "Type",
+      field: "Type",
+    },
+    {
+      label: "Manager Id",
+      field: "ManagerId",
+    },
+    {
+      label: "Start date",
+      field: "StartDate",
+    },
+    {
+      label: "Rating",
+      field: "Rating",
+    },
+  ];
   return (
     <div>
       <div className="text-2xl font-semibold">Table Component</div>
       <div className="text-gray-400 text-sm">RcTable Basic table</div>
       <span>A table displays data items in a tabular format..</span>
       <span>Simple Table with Javascript Array data.</span>
-      <div className="p-3 my-2 border h-96 overflow-auto">
-        {isFetching && <RcTable />}
+      <div className="my-2 overflow-auto">
+        {isSuccess && (
+          <RcTable
+            classes="h-96 border"
+            data={data}
+            columns={columns}
+            options={options}
+          >
+            <template
+              render={(row: any) => (
+                <>
+                  <td className="px-3 py-2">{row.DepartmentId}</td>
+                  <td className="px-3 py-2">{row.DepartmentName}</td>
+                  <td className="px-3 py-2">{row.Type}</td>
+                  <td className="px-3 py-2">{row.ManagerId}</td>
+                  <td className="px-3 py-2">{row.StartDate}</td>
+                  <td className="px-3 py-2">{row.Rating}</td>
+                </>
+              )}
+            ></template>
+          </RcTable>
+        )}
+
+        {/* <div role="status" className="max-w-sm animate-pulse">
+          <div className="h-6 bg-gray-200 rounded-full dark:bg-gray-700 w-48 mb-4"></div>
+          <div className="h-2 bg-gray-200 rounded-full dark:bg-gray-700 max-w-[360px] mb-2.5"></div>
+          <div className="h-2 bg-gray-200 rounded-full dark:bg-gray-700 mb-2.5"></div>
+          <div className="h-2 bg-gray-200 rounded-full dark:bg-gray-700 max-w-[330px] mb-2.5"></div>
+          <div className="h-2 bg-gray-200 rounded-full dark:bg-gray-700 max-w-[300px] mb-2.5"></div>
+          <div className="h-2 bg-gray-200 rounded-full dark:bg-gray-700 max-w-[360px]"></div>
+          <span className="sr-only">Loading...</span>
+        </div> */}
       </div>
     </div>
   );
--- src/React-components/Table/RcTable.tsx
+++ src/React-components/Table/RcTable.tsx
@@ -1,5 +1,47 @@
-const RcTable = () => {
-  return <div>RcTable</div>;
+declare module "react" {
+  interface HTMLAttributes<T> extends AriaAttributes, DOMAttributes<T> {
+    // extends React's HTMLAttributes
+    render?: any;
+  }
+}
+
+type RcTableOptions = {};
+
+type Props = {
+  children: JSX.Element;
+  data: Array<Object>;
+  columns: Array<Object>;
+  options: RcTableOptions;
+  classes?: string;
+};
+const RcTable = ({ data, columns, children, classes }: Props) => {
+  return (
+    <>
+      <div className={classes + " pl-1"}>
+        <table className="min-w-max  w-full box-border bg-white">
+          <thead className="overflow-hidden bg-inherit sticky top-0">
+            <tr className="text-left  bg-inherit border">
+              {columns.map((column: any) => (
+                <th
+                  className="font-semibold px-3 py-1 overflow-hidden"
+                  key={column.field}
+                >
+                  {column.label}
+                </th>
+              ))}
+            </tr>
+          </thead>
+          <tbody className="overflow-hidden">
+            {data.map((tableItem: any, index: number) => {
+              let obj = { ...tableItem };
+              obj.key = index;
+              return <tr key={tableItem.id}> {children.props.render(obj)}</tr>;
+            })}
+          </tbody>
+        </table>
+      </div>
+    </>
+  );
 };
 
 export default RcTable;
