8c2d10784017e1ed7ead501476f4c6a7101a34f0
Bharath3981 <bharath.baisetty@gmail.com>
2023-10-01 20:56:53 +0530

    tetw

--- src/React-components/Table/BasicTable.tsx
+++ src/React-components/Table/BasicTable.tsx
@@ -35,7 +35,7 @@ const BasicTable = () => {
       <div className="text-2xl font-semibold">Table Component</div>
       <div className="text-gray-400 text-sm">RcTable Basic table</div>
       <span>A table displays data items in a tabular format..</span>
-      <span>Simple Table with Javascript Array data.</span>
+      <span>Simple Table with Javascript Array data. SOme dummy</span>
       <div className="my-2 overflow-auto">
         {isSuccess && (
           <RcTable
