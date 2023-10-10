f1b37d4dd19cd902e59526f18c4ca235ea41e0eb
Bharath3981 <bharath.baisetty@gmail.com>
2023-10-10 15:14:59 +0530

    Test

--- src/React-components/Table/BasicTable.tsx
+++ src/React-components/Table/BasicTable.tsx
@@ -56,7 +56,7 @@ const BasicTable = () => {
   return (
     <div>
       <div className="text-2xl font-semibold">Table Component</div>
-      <div className="text-gray-400 text-sm">RcTable Basic table</div>
+      <div className="text-gray-400 text-sm">RcTable Basic</div>
       <span>A table displays data items in a tabular format.</span>
       <span>Simple Table with Javascript Array data.</span>
       <div>Selected rows: {JSON.stringify(selectedRows)}</div>
