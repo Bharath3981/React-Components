3a636a3aa1f3782fd851e326e7dddcbdb36ed0f8
Bharath3981 <bharath.baisetty@gmail.com>
2023-11-10 14:14:16 +0530

    Added

--- src/React-topics/useStateTopic/UseStateTopic.tsx
+++ src/React-topics/useStateTopic/UseStateTopic.tsx
@@ -1,7 +1,16 @@
+import { useState } from "react";
 import CodeSnippet from "../../util/CodeSnippet";
 import InfoBlock from "../../util/InfoBlock";
 
 const UseStateTopic = () => {
+  const [name, setName] = useState("");
+  const [previousName, setPreviousName] = useState("");
+
+  const [array, setArray] = useState([1, 2, 3, 4, 5]);
+  const [obj, setObj] = useState({
+    name: "Bharath",
+    place: "Anakapalli",
+  });
   return (
     <div>
       <h1 className="main-title">useState</h1>
@@ -30,6 +39,51 @@ const UseStateTopic = () => {
       <InfoBlock infoType="info">
         <p>ome info</p>
       </InfoBlock>
+      <h1 className="sub-title">Example previous value:</h1>
+      <div className="flex">
+        <label>Name: </label>
+        <input
+          type="text"
+          onChange={(event: any) => {
+            setName((name) => {
+              setPreviousName((val) => name);
+              return event.target.value;
+            });
+          }}
+        />
+      </div>
+      <div>Current Value: {name}</div>
+      <div>Previous Value: {previousName}</div>
+      <h1 className="sub-title">Example array update:</h1>
+      <CodeSnippet>{`<button
+          onClick={() => {
+            setArray((array) => [...array, array.length + 1]);
+          }}>Update Array</button>`}</CodeSnippet>
+      <div className="flex">
+        <span>Current Array: </span>
+        <span className="px-2">{JSON.stringify(array)}</span>
+        <button
+          onClick={() => {
+            setArray((array) => [...array, array.length + 1]);
+          }}
+        >
+          Update Array
+        </button>
+      </div>
+      <h1 className="sub-title">Example object update:</h1>
+      <div className="flex">
+        <span>Current Array: </span>
+        <span className="px-2">{JSON.stringify(obj)}</span>
+        <button
+          onClick={() => {
+            setObj((obj) => {
+              return { ...obj, place: "Visakhaptnam" };
+            });
+          }}
+        >
+          Update Object
+        </button>
+      </div>
     </div>
   );
 };
