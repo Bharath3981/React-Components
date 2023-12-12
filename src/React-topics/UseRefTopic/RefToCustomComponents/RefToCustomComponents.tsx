7275d028baecef7b4bfb6d1fa4636dcba44e90da
Bharath3981 <bharath.baisetty@gmail.com>
2023-12-12 06:38:19 +0530

    TEst

--- src/React-topics/UseRefTopic/RefToCustomComponents/RefToCustomComponents.tsx
+++ src/React-topics/UseRefTopic/RefToCustomComponents/RefToCustomComponents.tsx
@@ -2,14 +2,16 @@ import React, { useState, useRef, forwardRef } from "react";
 
 const RefToCustomComponents = () => {
   const [value, setValue] = useState<string>("Test");
-  const inputRef = useRef(null);
+  const inputRef = useRef<any>(null);
   function onChangeEvent(e: React.ChangeEvent<HTMLElement>) {
     setValue((e.target as HTMLInputElement).value);
+    console.log(inputRef.current);
   }
   return (
     <div className="pb-5">
       <h1 className="sub-title">I can’t get a ref to a custom component</h1>
       <MyInput value={value} ref={inputRef} onChange={onChangeEvent} />
+      <span>Value: {inputRef.current?.value}</span>
     </div>
   );
 };
