d21334fbdcf9a5357cbb5190d36b87d280b69633
Bharath3981 <bharath.baisetty@gmail.com>
2023-12-12 05:55:17 +0530

    Forward ref added in userRef

--- src/React-topics/UseRefTopic/RefToCustomComponents/RefToCustomComponents.tsx
+++ src/React-topics/UseRefTopic/RefToCustomComponents/RefToCustomComponents.tsx
@@ -1,14 +1,15 @@
-import React, { useState } from "react";
+import React, { useState, useRef, forwardRef } from "react";
 
 const RefToCustomComponents = () => {
   const [value, setValue] = useState<string>("Test");
+  const inputRef = useRef(null);
   function onChangeEvent(e: React.ChangeEvent<HTMLElement>) {
     setValue((e.target as HTMLInputElement).value);
   }
   return (
     <div className="pb-5">
       <h1 className="sub-title">I can’t get a ref to a custom component</h1>
-      <MyInput value={value} onChange={onChangeEvent} />
+      <MyInput value={value} ref={inputRef} onChange={onChangeEvent} />
     </div>
   );
 };
@@ -18,8 +19,8 @@ type MyInputType = {
   onChange: React.ChangeEventHandler<HTMLInputElement>;
 };
 
-function MyInput({ value, onChange }: MyInputType) {
-  return <input value={value} onChange={onChange} />;
-}
+const MyInput = forwardRef(({ value, onChange }: MyInputType, ref) => {
+  return <input value={value} onChange={onChange} ref={ref} />;
+});
 
 export default RefToCustomComponents;
