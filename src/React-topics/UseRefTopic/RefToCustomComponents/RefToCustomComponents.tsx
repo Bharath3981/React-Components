ac621eb24831ccbbdf068d059776c258b94afc43
Bharath3981 <bharath.baisetty@gmail.com>
2023-12-13 18:45:45 +0530

    te

--- src/React-topics/UseRefTopic/RefToCustomComponents/RefToCustomComponents.tsx
+++ src/React-topics/UseRefTopic/RefToCustomComponents/RefToCustomComponents.tsx
@@ -9,7 +9,7 @@ const RefToCustomComponents = () => {
   }
   return (
     <div className="pb-5">
-      <h1 className="sub-title">I can’t get a ref to a custom component 12</h1>
+      <h1 className="sub-title">I can’t get a ref to a custom component</h1>
       <MyInput value={value} ref={inputRef} onChange={onChangeEvent} />
       <span>Value: {inputRef.current?.value}</span>
     </div>
