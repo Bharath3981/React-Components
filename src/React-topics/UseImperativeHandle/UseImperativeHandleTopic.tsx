ebc1d555b201dd890a19b3c66882fffdda45dac8
Bharath3981 <bharath.baisetty@gmail.com>
2023-12-30 20:33:30 +0530

    fd

--- src/React-topics/UseImperativeHandle/UseImperativeHandleTopic.tsx
+++ src/React-topics/UseImperativeHandle/UseImperativeHandleTopic.tsx
@@ -5,7 +5,7 @@ const UseImperativeHandleTopic = () => {
     <div className="pb-5">
       <h1 className="main-title">UseImperativeHandle</h1>
       <p className="description">
-        <code>useImperativeHandle</code> is a React Hook that lets you customize te
+        <code>useImperativeHandle</code> is a React Hook that lets you customize
         the handle exposed as a ref.
       </p>
     </div>
