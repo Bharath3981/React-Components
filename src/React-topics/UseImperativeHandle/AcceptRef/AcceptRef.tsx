8084fa21a8c8c84514c7f0cad83a44f0c1d99de3
Bharath3981 <bharath.baisetty@gmail.com>
2024-01-01 19:30:30 +0530

    Implementing useImperative handle hook

--- src/React-topics/UseImperativeHandle/AcceptRef/AcceptRef.tsx
+++ src/React-topics/UseImperativeHandle/AcceptRef/AcceptRef.tsx
@@ -1,4 +1,5 @@
 import { forwardRef, useRef, useImperativeHandle } from "react";
+import CodeSnippet from "../../../util/CodeSnippet";
 
 const MyInput = forwardRef(function MyInput(props: any, ref) {
   useImperativeHandle(
@@ -25,6 +26,26 @@ const AcceptRef = () => {
       Value: {ref?.current?.value}
       <h1 className="sub-title">Accept Ref</h1>
       <MyInput type="text" placeholder="Enter some text" ref={ref} />
+      <CodeSnippet>{`const MyInput = forwardRef(function MyInput(props: any, ref) {
+        useImperativeHandle(
+          ref, () => {
+            return {
+              focus() {
+                ref?.current.focus();
+              },
+              scrollIntoView() {
+                ref?.current.scrollIntoView();
+              },
+            };
+          },
+          []);
+        return <input {...props} ref={ref} />;
+      });`}</CodeSnippet>
+      <p className="description">
+        Now, if the parent component gets a ref to MyInput, it will be able to
+        call the focus and scrollIntoView methods on it. However, it will not
+        have full access to the underlying <code>input</code> DOM node.
+      </p>
     </div>
   );
 };
