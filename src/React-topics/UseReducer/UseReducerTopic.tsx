0137713f13ff8251a3274cad947729e8a58ece54
Bharath3981 <bharath.baisetty@gmail.com>
2024-01-04 07:33:11 +0530

    Update useReducer

--- src/React-topics/UseReducer/UseReducerTopic.tsx
+++ src/React-topics/UseReducer/UseReducerTopic.tsx
@@ -1,7 +1,11 @@
 import React from "react";
 
 const UseReducerTopic = () => {
-  return <div>UseReducerTopic</div>;
+  return (
+    <div className="pb-5">
+      <h1 className="main-title">UseReducer</h1>
+    </div>
+  );
 };
 
 export default UseReducerTopic;
