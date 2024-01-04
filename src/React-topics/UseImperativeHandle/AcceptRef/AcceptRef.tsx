79691003c27e89b8da7261b67c186d5e8f23e326
Bharath3981 <bharath.baisetty@gmail.com>
2024-01-04 07:18:52 +0530

    UseImperative handle

--- src/React-topics/UseImperativeHandle/AcceptRef/AcceptRef.tsx
+++ src/React-topics/UseImperativeHandle/AcceptRef/AcceptRef.tsx
@@ -2,30 +2,37 @@ import { forwardRef, useRef, useImperativeHandle } from "react";
 import CodeSnippet from "../../../util/CodeSnippet";
 
 const MyInput = forwardRef(function MyInput(props: any, ref: any) {
+  const myInput: any = useRef(null);
   useImperativeHandle(
     ref,
     () => {
       return {
         focus() {
-          ref?.current.focus();
+          myInput?.current.focus();
         },
         scrollIntoView() {
-          ref?.current.scrollIntoView();
+          myInput?.current.scrollIntoView();
         },
       };
     },
     []
   );
-  return <input {...props} ref={ref} />;
+  return <input {...props} ref={myInput} />;
 });
 const AcceptRef = () => {
   const ref = useRef(null);
   console.log(ref);
+
+  const focusTextBox = () => {
+    console.log(ref);
+    ref.current.focus();
+  };
   return (
     <div>
       Value: {ref?.current?.value}
       <h1 className="sub-title">Accept Ref</h1>
-      <MyInput type="text" placeholder="Enter some text" ref={ref} />
+      <MyInput type="text" placeholder="Enter some text" ref={ref} />{" "}
+      <button onClick={() => focusTextBox()}>Focus</button>
       <CodeSnippet>{`const MyInput = forwardRef(function MyInput(props: any, ref) {
         useImperativeHandle(
           ref, () => {
