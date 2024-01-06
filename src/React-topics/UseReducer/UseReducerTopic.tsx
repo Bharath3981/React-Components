49a7778f4cfe98f975019a0cb2828757399665a4
Bharath3981 <bharath.baisetty@gmail.com>
2024-01-06 21:13:13 +0530

    Implementing usereducer

--- src/React-topics/UseReducer/UseReducerTopic.tsx
+++ src/React-topics/UseReducer/UseReducerTopic.tsx
@@ -1,7 +1,40 @@
+import { useReducer } from "react";
+
+function reducer(state: any, action: any) {
+  switch (action) {
+    case "increaseAge":
+      return {
+        age: state.age + 1,
+        name: state.name,
+      };
+      break;
+    case "decreaseAge":
+      return {
+        age: state.age - 1,
+        name: state.name,
+      };
+      break;
+    case "updateName":
+      return {
+        age: state.age,
+        name: state.name,
+      };
+      break;
+  }
+}
+
 const UseReducerTopic = () => {
+  const [state, dispatch] = useReducer(reducer, {
+    age: 5,
+    name: "Bharath Baisetty",
+  });
   return (
     <div className="pb-5">
       <h1 className="main-title">UseReducer</h1>
+      <p className="description">
+        <code>useReducer</code> is a React Hook that lets you add a reducer to
+        your component.
+      </p>
     </div>
   );
 };
