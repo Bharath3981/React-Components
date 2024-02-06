757150f972477ede990949b03ef9d5de5cf3205e
Bharath3981 <bharath.baisetty@gmail.com>
2024-02-06 21:47:37 +0530

    Debouncing implemented

--- src/JS-topics/DebouncingTopic/DebouncingTopic.tsx
+++ src/JS-topics/DebouncingTopic/DebouncingTopic.tsx
@@ -4,14 +4,32 @@ const DebouncingTopic = () => {
   const albums: any = useRef([]);
   const [filteredList, setFilteredList] = useState([]);
   useEffect(() => {
+    console.log("useEffect called");
+    let albumsFetched = false;
     const getAlbums = async () => {
+      console.log("getAlbum calle");
       const res = await fetch("https://jsonplaceholder.typicode.com/photos");
       const data = await res.json();
       albums.current = data;
-      setFilteredList(data);
+      if (!albumsFetched) {
+        setFilteredList(data);
+      }
     };
     getAlbums();
+    return () => {
+      albumsFetched = true;
+    };
   }, []);
+
+  let timeout: number = 0;
+  const debouncing = (e: any) => {
+    if (timeout) {
+      clearTimeout(timeout);
+    }
+    timeout = setTimeout(() => {
+      filterAlbums(e);
+    }, 400);
+  };
 
   const filterAlbums = (e: any) => {
     console.log(e.target.value);
@@ -24,6 +42,19 @@ const DebouncingTopic = () => {
       setFilteredList(albums.current);
     }
   };
+  const someMagic = (fn: any, d: number) => {
+    let timer: any = 0;
+    return (e: any) => {
+      if (timer) {
+        clearTimeout(timer);
+      }
+      timer = setTimeout(() => {
+        fn(e);
+      }, d);
+    };
+  };
+
+  const debouncing1 = someMagic(filterAlbums, 400);
   return (
     <div>
       <div className="main-title">Debouncing</div>
@@ -34,9 +65,10 @@ const DebouncingTopic = () => {
         triggered by rapid events, such as keystrokes or scroll movements.
       </p>
       <div className="side-title">Albums</div>
+      <input type="text" onChange={(e) => debouncing(e)} placeholder="search" />
       <input
         type="text"
-        onChange={(e) => filterAlbums(e)}
+        onChange={(e) => debouncing1(e)}
         placeholder="search"
       />
       <ul className="border-2 border-black-500 h-80 overflow-auto rounded-md p-2 mt-3">
