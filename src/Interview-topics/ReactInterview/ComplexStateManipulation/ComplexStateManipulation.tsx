1ef29eab44fa27d88146bb3df7659be1fb14e17d
Bharath3981 <bharath.baisetty@gmail.com>
2024-02-10 22:07:23 +0530

    impleme google sheet

--- src/Interview-topics/ReactInterview/ComplexStateManipulation/ComplexStateManipulation.tsx
+++ src/Interview-topics/ReactInterview/ComplexStateManipulation/ComplexStateManipulation.tsx
@@ -7,10 +7,31 @@ const ComplexStateManipulation = () => {
     option[option.id] = false;
   });
   const [list, setList] = useState(Options.categories);
+  const postData = async () => {
+    let postdata = {
+      id: 123,
+      first_name: "Adithya",
+      last_name: "Baisetty",
+      gender: "male",
+      married: false,
+      mobile_no: "998902648",
+      pincode: 565654,
+    };
+    const req = await fetch(
+      "https://script.google.com/macros/s/AKfycbxAaNjlqFGZoMa7VTKnoBf_YnQ6kxx9BoewyjZL8X_nsX6PkA4-Z0PzXb5qcvpCSBy9/exec?first_name=Adithya&last_name=Baisetty&gender=male&married=false&mobile_no=99889989&pincode=5656565",
+      {
+        method: "GET",
+        mode: "cors",
+      }
+    );
+    const response = await req.json();
+    console.log(response);
+  };
   return (
     <div>
       <div className="main-title">Complex state manipulation</div>
       <div className="pt-3">
+        <button onClick={() => postData()}>Post data</button>
         <RcMultiSelectBox
           startingText={"No options selected"}
           list={list}
